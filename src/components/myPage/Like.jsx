fc8fca5638e7ee5ca1e8b3ce312f55f740bacbd1
choiinyoung <choi121100@naver.com>
2023-07-02 18:33:12 +0900

    Refactor : Update.jsx 닉네임 중복수정
    Refactor : Update.jsx 닉네임 중복수정

--- src/components/myPage/Like.jsx
+++ src/components/myPage/Like.jsx
@@ -4,82 +4,88 @@ import { getLike } from '../../apis/mypage';
 import { GoChevronLeft, GoChevronRight } from 'react-icons/go';
 
 export default function Like() {
-  const [userTitle, setUserTitle] = useState();
-  const [userDate, setUserDate] = useState();
-  const [userPrice, setUserPrice] = useState();
+  const [itemLike, setItemsLike] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   const getLikeInfo = async () => {
     try {
       const userId = sessionStorage.getItem('userId');
       const resLike = await getLike(userId);
       const dbLikeInfo = resLike.data;
-      console.log(dbLikeInfo[0]);
-      setUserTitle(dbLikeInfo[0].item_title);
-      setUserDate(dbLikeInfo[0].item_date);
-      setUserPrice(dbLikeInfo[0].item_price);
+
+      const itemsPerPage = 4;
+      const startIndex = currentPage * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+
+      console.log(dbLikeInfo);
+
+      let filteredItems = dbLikeInfo;
+
+      const itemsToShow = filteredItems.slice(startIndex, endIndex);
+
+      const renderedItems = itemsToShow.map((item) => (
+        <tr key={item.itemId}>
+          <th>
+            <input type="checkbox"></input>
+          </th>
+          <th>{item.title}</th>
+          <th>{item.price}</th>
+          <th>{item.user}</th>
+        </tr>
+      ));
+
+      setItemsLike(renderedItems);
+      setTotalPages(Math.ceil(filteredItems.length / itemsPerPage));
     } catch (error) {
       console.error(error);
     }
   };
+
   useEffect(() => {
     getLikeInfo();
   }, []);
+
+  const PageNumbers = () => {
+    const numbers = [];
+    for (let i = 0; i < totalPages; i++) {
+      numbers.push(
+        <p
+          key={i}
+          className={currentPage === i ? 'active' : ''}
+          onClick={() => setCurrentPage(i)}
+        >
+          {i + 1}
+        </p>,
+      );
+    }
+    return <div className="page-numbers">{numbers}</div>;
+  };
 
   return (
     <>
       <div className="like">
         <div className="content">
-          <table border={0}>
-            <thead>
-              <tr>
-                <th>
-                  <input type="checkbox"></input>
-                </th>
-                <th>상품정보</th>
-                <th>거래일자</th>
-                <th>거래금액</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <input type="checkbox"></input>
-                </td>
-                <td>{userTitle}</td>
-                <td>{userDate}</td>
-                <td>{userPrice}</td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox"></input>
-                </td>
-                <td>제목</td>
-                <td>날자</td>
-                <td>금액</td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox"></input>
-                </td>
-                <td>제목</td>
-                <td>날자</td>
-                <td>금액</td>
-              </tr>
-              <tr>
-                <td>
-                  <input type="checkbox"></input>
-                </td>
-                <td>제목</td>
-                <td>날자</td>
-                <td>금액</td>
-              </tr>
-            </tbody>
-          </table>
+          {itemLike.length > 0 ? (
+            <table border={0}>
+              <thead>
+                <tr>
+                  <th>
+                    <input type="checkbox"></input>
+                  </th>
+                  <th>상품정보</th>
+                  <th>상품가격</th>
+                  <th>사용자</th>
+                </tr>
+              </thead>
+              <tbody>{itemLike}</tbody>
+            </table>
+          ) : (
+            <p className="msg">관심상품이 없습니다</p>
+          )}
 
-          <div className="button">
-            <GoChevronLeft size={50} className="btn" />
-            <GoChevronRight size={50} className="btn" />
-          </div>
+          {itemLike.length > 0 && <PageNumbers />}
         </div>
       </div>
     </>
