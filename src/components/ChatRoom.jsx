3aa639815b5e2a769b30d3e9ef3ba6b49a5eff1b
HeeHeeHee <praaa0220@gmail.com>
2023-07-26 22:54:44 +0900

    Refactor: 채팅

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -7,49 +7,60 @@ const ChatRoom = ({ sellerID }) => {
   const [messages, setMessages] = useState([]); // 메시지 목록을 담을 상태 변수
   const [connected, setConnected] = useState(false); // 연결 상태를 담을 상태 변수
   const [userImg, setUserImg] = useState();
+  const [userNickName, setUserNickName] = useState();
+  const [roomId, setRoomId] = useState(null); // 채팅방 식별자를 담을 상태 변수
   const clientRef = useRef(null); // SockJsClient 컴포넌트의 참조를 담을 변수
 
   useEffect(() => {
     const getData = async () => {
       try {
+        // 채팅방 생성 또는 입장 요청
+        const response = await axios.post('/chat/room', {
+          sellerId: sellerID.split('@')[0],
+          // buyerId: sessionStorage.getItem('userId').split('/')[0],
+          buyerId: 'test',
+        });
+        setRoomId(response.data.roomId);
+
         const userResponse = await axios.get('/user/userInfo', {
           params: {
             userId: sessionStorage.getItem('userId'),
           },
         });
         const dbUserInfo = userResponse.data;
+        console.log(dbUserInfo);
         // 프로필 이미지
         setUserImg(dbUserInfo[0].user_img);
-
+        setUserNickName(dbUserInfo[0].user_nickname);
         // 채팅 정보 가져오기
-        // 채팅 받는 사람 아이디 = 판매자 아이디
-        const messageResponse = await axios.get('/chat/messages', {
-          params: {
-            sellerId: sellerID,
-          },
-        });
-        const dbMessages = messageResponse.data;
-        setMessages(dbMessages);
+        // const messageResponse = await axios.get(
+        //   `/chat/messages/${response.data.roomId}`,
+        // );
+        // const dbMessages = messageResponse.data;
+        // setMessages(dbMessages);
       } catch (error) {
         console.error(error);
       }
     };
 
     getData();
-  }, []);
+  }, [sellerID]);
 
   const handleSendMessage = (message) => {
     // connected로 현재 웹소켓 연결 상태를 확인
     if (connected) {
       // 사용자 ID와 입력된 메시지를 담은 newMessage 객체를 생성
       const newMessage = {
-        userId: sessionStorage.getItem('userId'),
+        userId: userNickName,
         message: message,
       };
       // 서버로 메시지를 전송
-      // '/app/message'는 백엔드에서 웹소켓 메시지를 처리하는 엔드포인트 주소
-      // newMessage 객체를 JSON 문자열로 변환하여 전송
-      clientRef.current.sendMessage('/app/message', JSON.stringify(newMessage));
+      console.log('웹소캣이 연결되었습니다.');
+
+      clientRef.current.sendMessage(
+        `/app/message/${roomId}`,
+        JSON.stringify(newMessage),
+      );
     } else {
       console.log('WebSocket 연결되지 않았습니다.');
     }
@@ -57,6 +68,7 @@ const ChatRoom = ({ sellerID }) => {
 
   const handleMessageReceive = (message) => {
     // 새로운 메시지를 받았을 때 실행될 콜백 함수
+    console.log('메시지:', message);
     setMessages((prevMessages) => [...prevMessages, message]); // 새로운 메시지를 상태에 추가
   };
 
@@ -71,17 +83,23 @@ const ChatRoom = ({ sellerID }) => {
   };
 
   return (
-    <div>
+    <div className="chat_container">
       {messages.map((message, index) => (
-        <div key={index}>
-          <strong>{message.user}: </strong>
-          {message.message}
+        <div
+          className={message.userId === userNickName ? 'me' : 'you'}
+          key={index}
+        >
+          <div className="chat_content">
+            <p>
+              <strong>{message.userId}: </strong>
+            </p>
+            <p>{message.message}</p>
+          </div>
         </div>
       ))}
       {/* 메시지 목록을 반복하여 렌더링 */}
-
       <form
-        className="chat_container"
+        className="chat_form"
         onSubmit={(e) => {
           e.preventDefault();
           const message = e.target.elements.message.value;
@@ -111,21 +129,18 @@ const ChatRoom = ({ sellerID }) => {
         <button type="submit">전송</button>
       </form>
       {/* 메시지 입력 폼과 전송 버튼을 포함한 폼 요소 */}
-
-      <SockJsClient
-        url="/chat"
-        topics={['/topic/chat']}
-        onMessage={handleMessageReceive} // 메시지 수신
-        onConnect={handleConnect} // 연결 성공
-        onDisconnect={handleDisconnect} // 연결 종료
-        ref={clientRef}
-        debug={false}
-      />
+      {roomId && ( // roomId가 존재할 때만 WebSocket을 연결
+        <SockJsClient
+          url="http://localhost:8080/ws"
+          topics={[`/topic/chat/${roomId}`]} // 채팅방별로 토픽을 구분
+          onMessage={handleMessageReceive} // 메시지 수신
+          onConnect={handleConnect} // 연결 성공
+          onDisconnect={handleDisconnect} // 연결 종료
+          ref={clientRef}
+          debug={false}
+        />
+      )}
       {/* SockJsClient 컴포넌트를 사용하여 WebSocket 통신 설정 */}
-
-      {/* url 속성을 통해 웹소켓 서버의 주소를 설정 */}
-      {/* topics 속성은 구독할 웹소켓 토픽(topic)을 설정하고 클라이언트가 메시지를 수신하고자 하는 대상을 지정하는데 사용 */}
-      {/* /topic/chat 토픽을 구독하겠다는 의미 */}
     </div>
   );
 };
